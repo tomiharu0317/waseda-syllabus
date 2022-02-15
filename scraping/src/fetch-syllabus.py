4076973eb6f1e8fe703ca2282334eac30306badf
tomiharu0317 <tomiharu1017@gmail.com>
2022-02-15 14:07:58 +0900

    Setup docker env for scraping.

--- scraping/src/fetch-syllabus.py
+++ scraping/src/fetch-syllabus.py
@@ -1,12 +1,11 @@
 import os
 
 from selenium import webdriver
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 
 driver = webdriver.Remote(
     command_executor=os.environ["SELENIUM_URL"],
-    desired_capabilities=DesiredCapabilities.FIREFOX.copy()
+    options=webdriver.FirefoxOptions()
 )
 driver.implicitly_wait(5)
 
@@ -14,3 +13,4 @@ driver.get("https://www.time-j.net/worldtime/country/jp")
 
 print(driver.find_element_by_xpath("/html/body/div/div[6]/div[1]/div/p[5]").text)
 driver.quit()
+
